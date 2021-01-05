5b4e54a7577f9723874645ee620cbc0dddcdc643
Saurabh Kabadi <68393690+Saurabh-DS@users.noreply.github.com>
2021-01-05 15:37:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,41 +1,130 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
+import ortools
+from ortools.constraint_solver import routing_enums_pb2
+from ortools.constraint_solver import pywrapcp
 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
+# model = pickle.load(open('model.pkl', 'rb'))
+
+data = {}
+data['distance_matrix'] = []
+data['vehicle_capacities'] = []
+data['demands'] = []
+# data['num_vehicles'] = []
+# data['depot'] = []
 
 @app.route('/')
 def home():
-    return render_template('LoanPrediction2.html')
+    return render_template('OptimalRoute.html')
 
-@app.route('/predict',methods=['POST'])
-def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
-    int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
+# @app.route('/predict',methods=['POST'])
+# def predict():
+#     '''
+#     For rendering results on HTML GUI
+#     '''
+#     data['distance_matrix'] = request.form.get('distance_matrix')
+#     data['vehicle_capacities'] = request.form.get('vehicle_capacities')
+#     data['demands'] = request.form.get('demands')
+#     data['num_vehicles'] = request.form.get('num_vehicles')
+#     data['depot'] = request.form.get('depot')
 
-    output = round(prediction[0], 2)
-    if output == 1:
-        return render_template('LoanPrediction2.html', prediction_text="Congratulations!!! Your Loan Will Be Approved!")
-    else:
-        return render_template('LoanPrediction2.html', prediction_text="Sorry! Your Loan Cannot Be Approved!")
+@app.route('/',methods=['POST'])
+def create_data_model():
+    data['distance_matrix'] = request.form.get('distance_matrix')
+    data['vehicle_capacities'] = request.form.get('vehicle_capacities')
+    data['demands'] = request.form.get('demands')
+    data['num_vehicles'] = request.form.get('num_vehicles')
+    data['depot'] = request.form.get('depot')
+    return data
 
-    # return render_template('LoanPrediction2.html', prediction_text='Your loan will be $ {}'.format(output))
+def print_solution(data, manager, routing, solution):
+    total_distance = 0
+    total_load = 0
+    for vehicle_id in range(data['num_vehicles']):
+        index = routing.Start(vehicle_id)
+        plan_output = 'Route for vehicle {}:\n'.format(vehicle_id)
+        route_distance = 0
+        route_load = 0
+        while not routing.IsEnd(index):
+            node_index = manager.IndexToNode(index)
+            route_load += data['demands'][node_index]
+            plan_output += ' {0} Load({1}) -> '.format(node_index, route_load)
+            previous_index = index
+            index = solution.Value(routing.NextVar(index))
+            route_distance += routing.GetArcCostForVehicle(
+                previous_index, index, vehicle_id)
+        plan_output += ' {0} Load({1})\n'.format(manager.IndexToNode(index),
+                                                 route_load)
+        plan_output += 'Distance of the route: {}m\n'.format(route_distance)
+        plan_output += 'Load of the route: {}\n'.format(route_load)
+        print(plan_output)
+        total_distance += route_distance
+        total_load += route_load
+    print('Total distance of all routes: {}m'.format(total_distance))
+    print('Total load of all routes: {}'.format(total_load))
 
-@app.route('/predict_api',methods=['POST'])
-def predict_api():
-    '''
-    For direct API calls trought request
-    '''
-    data = request.get_json(force=True)
-    prediction = model.predict([np.array(list(data.values()))])
 
-    output = prediction[0]
-    return jsonify(output)
+def main():
+    """Solve the CVRP problem."""
+    # Instantiate the data problem.
+    data = create_data_model()
+
+    # Create the routing index manager.
+    manager = pywrapcp.RoutingIndexManager(len(data['distance_matrix']),
+                                           data['num_vehicles'], data['depot'])
+
+    # Create Routing Model.
+    routing = pywrapcp.RoutingModel(manager)
+
+
+    # Create and register a transit callback.
+    def distance_callback(from_index, to_index):
+        """Returns the distance between the two nodes."""
+        # Convert from routing variable Index to distance matrix NodeIndex.
+        from_node = manager.IndexToNode(from_index)
+        to_node = manager.IndexToNode(to_index)
+        return data['distance_matrix'][from_node][to_node]
+
+    transit_callback_index = routing.RegisterTransitCallback(distance_callback)
+
+    # Define cost of each arc.
+    routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+
+
+    # Add Capacity constraint.
+    def demand_callback(from_index):
+        """Returns the demand of the node."""
+        # Convert from routing variable Index to demands NodeIndex.
+        from_node = manager.IndexToNode(from_index)
+        return data['demands'][from_node]
+
+    demand_callback_index = routing.RegisterUnaryTransitCallback(
+        demand_callback)
+    routing.AddDimensionWithVehicleCapacity(
+        demand_callback_index,
+        0,  # null capacity slack
+        data['vehicle_capacities'],  # vehicle maximum capacities
+        True,  # start cumul to zero
+        'Capacity')
+
+    # Setting first solution heuristic.
+    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+    search_parameters.first_solution_strategy = (
+        routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
+    search_parameters.local_search_metaheuristic = (
+        routing_enums_pb2.LocalSearchMetaheuristic.GUIDED_LOCAL_SEARCH)
+    search_parameters.time_limit.FromSeconds(1)
+
+    # Solve the problem.
+    solution = routing.SolveWithParameters(search_parameters)
+
+    return render_template('OptimalRoute.html', prediction_text=solution)
+
+    # # Print solution on console.
+    # if solution:
+    #     print_solution(data, manager, routing, solution)
 
 if __name__ == "__main__":
     app.run(debug=True)
